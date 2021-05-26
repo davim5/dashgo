c8f1f9b1627185e726e54bdfcb11701126e44a29
Davi Lima <davim5@hotmail.com>
2021-05-25 22:29:18 -0300

    Fluxo-de-navegacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Text, Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -20,15 +21,17 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-                        <Button
-                         as="a" 
-                         size="sm" 
-                         fontSize="small" 
-                         colorScheme="pink"
-                         leftIcon={<Icon as={RiAddLine}></Icon>}
-                         >
-                            Criar novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                            as="a" 
+                            size="sm" 
+                            fontSize="small" 
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine}></Icon>}
+                            >
+                                Criar novo
+                            </Button>
+                        </Link>
                     </Flex>
 
                     <Table colorScheme="whiteAlpha">
