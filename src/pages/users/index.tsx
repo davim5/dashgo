6131c5527168b96e2d40932b762cafb46d7ac640
Davi Lima <davim5@hotmail.com>
2021-06-08 10:18:22 -0300

    prefetch de dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,5 @@
-import { Text, Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from "next/link";
+import { Text, Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -9,6 +9,7 @@ import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from 'react-query';
 import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
     const [ page, setPage ] = useState(1);
@@ -20,6 +21,16 @@ export default function UserList() {
         base: false,
         lg: true,
     });
+
+    async function handlePrefetchUser(userId: string) {
+        await queryClient.prefetchQuery(['user',userId], async () => {
+            const response = await api.get(`users/${userId}`);
+
+            return response.data;
+        },{
+            staleTime: 1000 * 60 * 30,
+        })
+    }
 
     return(
         <Box>
@@ -35,7 +46,7 @@ export default function UserList() {
                             {!isLoading && isFetching && <Spinner size="sm" color="gray.500"/>}
                         </Heading>
 
-                        <Link href="/users/create" passHref>
+                        <NextLink href="/users/create" passHref>
                             <Button
                             as="a" 
                             size="sm" 
@@ -45,7 +56,7 @@ export default function UserList() {
                             >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
                     {isLoading ? (
@@ -79,7 +90,9 @@ export default function UserList() {
                                 </Td>
                                 <Td>
                                     <Box>
-                                        <Text fontWeight="bold">{user.name}</Text>
+                                        <Link color="purple.400" onMouseEnter = {() => handlePrefetchUser(user.id)}>
+                                            <Text fontWeight="bold">{user.name}</Text>
+                                        </Link>
                                         <Text fontSize="sm" color="gray.300">{user.email}</Text>
                                     </Box>
                                 </Td>
